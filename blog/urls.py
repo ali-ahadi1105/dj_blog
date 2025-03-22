5feb4a5a8a6bf01e593c7b24d59ee05013be9c6b
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-03-22 22:18:39 +0330

    add first view to blog app and create urls file for blog app

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import home, api
+
+
+app_name = "blog"
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('api/', api, name='api'),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse("Hello from django blog")
+
+
+def api(request):
+    data ={
+        "1": {
+            "title": "First article1",
+            "description": "First article in dj blog",
+            "id": 21
+        },
+        "2": {
+            "title": "Second article1",
+            "description": "Second article in dj blog",
+            "id": 22
+        },
+        "3": {
+            "title": "Third article1",
+            "description": "Third article in dj blog",
+            "id": 23
+        }
+    }
+    return JsonResponse(data)
