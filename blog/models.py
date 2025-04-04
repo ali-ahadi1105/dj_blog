b21eada946f3ed7f04438a71d5afa0bd53377b4c
ali-ahadi1105 <aliahadi.f1105@gmail.com>
2025-04-04 15:15:46 +0330

    create utils for change date and time to persian date and time

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from extentions.utils import jalali_converter
 
 # Create your models here.
 
@@ -25,3 +26,8 @@ class Article(models.Model):
     
     def __str__(self):
         return self.title
+    
+    def jpublish(self):
+        return jalali_converter(self.publish)
+    
+    jpublish.short_description = "زمان انتشار"
